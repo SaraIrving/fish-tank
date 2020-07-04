e0f37fd8003ce7978cd223a458397e62d65a02c7
Sara Irving <sarakirving@gmail.com>
2020-07-04 04:14:44 +0000

    Initial set up of BiteFish class

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,6 @@
+class BiteFish extends Fish {
+  constructor (options) {
+    super(options); //call super to run the code inside Fish's constructor
+    this.imageUri = '/images/bite-fish.gif' //set the image
+  }
+}
