ebbdacdf4412b4735191a2b806d5e2e91b68de6e
Sara Irving <sarakirving@gmail.com>
2020-07-04 05:00:07 +0000

    inital set up of Dolphin class

---
+++ public/scripts/entities/dolphin.js
@@ -0,0 +1,6 @@
+class Dolphin extends Fish {
+  constructor (options) {
+    super(options); //call super to run the code inside Fish's constructor
+    this.imageUri = '/images/dolphin.png' //set the image
+  }
+}
