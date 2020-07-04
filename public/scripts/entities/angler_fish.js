a16546b98a35f40068023b501d69773b43525685
Sara Irving <sarakirving@gmail.com>
2020-07-04 04:34:30 +0000

    Updated to use new png angler fish image

---
+++ public/scripts/entities/angler_fish.js
@@ -0,0 +1,7 @@
+class AnglerFish extends Fish {
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/angler-fish.png'; // Set the image
+  }
+
+}
